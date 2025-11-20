a9f67a455b4ab194cdd1be436ddc862de4ef60bc
Fidan Zeynalli <fidan.zeynalli@georgebrown.ca>
2025-11-20 16:12:09 -0500

    new api added

--- lab11-Docker/app.py
+++ lab11-Docker/app.py
@@ -33,5 +33,9 @@ def metrics():
 def greet():
     return "Welcome to Minikube, kubernetes and argoCD examples"
 
+@app.route("/welcome")
+def greet():
+    return "Welcome to GBC DevOps!"
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
